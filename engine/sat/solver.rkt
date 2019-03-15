2999acfde0beeb1408fc3c6c4aa7f31b5dfb92fb
Thomas <tbruceb@cs.washington.edu>
2019-03-14 20:00:36 -0700

    call lingeling as command

--- engine/sat/solver.rkt
+++ engine/sat/solver.rkt
@@ -5,7 +5,7 @@
 
 (provide (all-defined-out))
 
-(define solver-path "")
+(define solver-path "lingeling -q -T 30")
 (define solver-args (list "-q" "-T" "30"))
 
 (struct SAT (translator clauses unsat?) #:mutable)
@@ -51,7 +51,8 @@
     [else
      (printf "starting solver ~a...\n" solver-path)
      (match-define (list pout pin pid perr psig)
-       (apply (curry process* solver-path) solver-args))
+        (process solver-path)
+       #;(apply (curry process solver-path) solver-args))
      (SAT-write SAT pin)
      (close-output-port pin)
      (psig 'wait)
